15b4fc11583e345d91dd31b9a51bfd2d642990b7
Chris Breiding <chrisbreiding@gmail.com>
2015-02-03 10:44:54 -0500

    add show all episodes functionality

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('shows', { path: '/' });
+  this.resource('shows', { path: '/' }, function () {
+    this.resource('show', { path: '/:id' });
+  });
 });
 
 export default Router;
