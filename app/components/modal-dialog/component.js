2f158017d953fedb25682dfd98c8c2a1bef2969e
chrisbreiding <chrisbreiding@gmail.com>
2015-02-05 22:39:23 -0500

    keep background from scrolling when modal is present

--- app/components/modal-dialog/component.js
+++ app/components/modal-dialog/component.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Component.extend({
+
+  setup: function () {
+    $(document.body).addClass('modal-dialog-present');
+  }.on('didInsertElement'),
+
+  willDestroy: function () {
+    $(document.body).removeClass('modal-dialog-present');
+  },
 
   actions: {
     ok () {
