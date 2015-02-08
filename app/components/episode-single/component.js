99a60bfda2581f58f42e878b64d545d3611118dd
chrisbreiding <chrisbreiding@gmail.com>
2015-02-08 11:50:16 -0500

    improve file name text selection

--- app/components/episode-single/component.js
+++ app/components/episode-single/component.js
@@ -1,11 +1,26 @@
 import Ember from 'ember';
+import $ from 'jquery';
 import date from '../../utils/date';
 
 export default Ember.Component.extend({
 
   tagName: 'li',
   classNames: ['episode-single'],
-  classNameBindings: ['airdateStatus'],
+  classNameBindings: ['airdateStatus', 'showFileName'],
+
+  showFileName: false,
+
+  setup: function () {
+    $(document.body).on(`click.${this.get('episode.id')}`, () => {
+      if (!this.get('isDestroyed')) {
+        Ember.run(() => { this.send('hideFileName'); });
+      }
+    });
+  }.on('didInsertElement'),
+
+  willDestroy () {
+    $(document.body).off(`click.${this.get('episode.id')}`);
+  },
 
   airdateStatus: function () {
     let airdate = moment(this.get('episode.airdate'));
@@ -16,6 +31,31 @@ export default Ember.Component.extend({
            date.isFarFuture(airdate) ? 'far-future' :
            date.isFuture(airdate)    ? 'future' :
                                         'upcoming';
-  }.property('episode.airdate')
+  }.property('episode.airdate'),
+
+  actions: {
+    showFileName () {
+      this.set('active', true);
+      this.set('showFileName', true);
+
+      Ember.run.later(() => {
+        let text = this.$('.file-name')[0];
+        let selection = window.getSelection();
+        let range = document.createRange();
+
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+        this.set('active', false);
+      });
+    },
+
+    hideFileName () {
+      if (!this.get('active')) {
+        this.set('showFileName', false);
+      }
+    }
+  }
 
 });
