d0f2233c86756d52fb76c79ea51adbaf3efb079b
Chris Breiding <chrisbreiding@gmail.com>
2015-11-17 13:38:00 -0500

    add error handling to loading api key and retrieving shows

--- tv/TV Guide/MainViewController.swift
+++ tv/TV Guide/MainViewController.swift
@@ -5,29 +5,54 @@ import SwiftyJSON
 class MainViewController: UIViewController {
     @IBOutlet var textView: UITextView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
 
+        loadShows { shows in
+            self.textView.text = shows.map({ show in show["display_name"].stringValue }).joinWithSeparator("\n")
+        }
 
-        var apiKey: String = ""
+    }
+
+    func loadShows(onLoad: ([JSON] -> ())) {
+        guard let apiKey = loadApiKey() else { return }
+
+        Alamofire
+            .request(.GET, "http://tvapi.crbapps.com/shows", headers: [ "api_key": apiKey ])
+            .response { request, response, data, error in
+                let statusCode = Int((response?.statusCode)!)
+                if statusCode != 200 && statusCode != 304 {
+                    self.showErrorAlert("Error retrieving data", message: "Failed loading shows. Status code was \(statusCode).")
+                }
+            }
+            .responseData { response in
+                if let data = response.result.value {
+                    onLoad(JSON(data: data)["shows"].arrayValue)
+                }
+        }
+    }
+
+    func loadApiKey() -> String? {
+        var apiKey: String?
         if let path = NSBundle.mainBundle().pathForResource("secrets", ofType: "txt") {
             do {
                 apiKey = try String(contentsOfFile: path, encoding: NSUTF8StringEncoding)
-            } catch {}
-        }
-
-        let headers = [
-            "api_key": apiKey
-        ]
-
-        Alamofire
-            .request(.GET, "http://tvapi.crbapps.com/shows", headers: headers)
-            .responseData { response in
-                if let data = response.result.value {
-                    let json = JSON(data: data)
-                    let shows = json["shows"].arrayValue
-                    self.textView.text = shows.map({ show in show["display_name"].stringValue }).joinWithSeparator("\n")
+            } catch {
+                showErrorAlert("Error retrieving API Key", message: "Could not load contents of secrets file")
             }
+        } else {
+            showErrorAlert("Error retrieving API Key", message: "Could not load secrets file")
         }
+
+        return apiKey
+    }
+
+    func showErrorAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: { _ in
+            alert.removeFromParentViewController()
+        })
+        alert.addAction(action)
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 }
