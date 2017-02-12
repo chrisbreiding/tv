0b5bffc12346ddca2e1131daf11e400797569c71
Chris Breiding <chrisbreiding@gmail.com>
2017-02-12 13:00:14 -0500

    make mobx strict

--- desktop/src/main.jsx
+++ desktop/src/main.jsx
@@ -1,5 +1,8 @@
+import { useStrict } from 'mobx'
 import React from 'react'
 import { render } from 'react-dom'
 import App from './app/app'
+
+useStrict(true)
 
 render(<App />, document.getElementById('app'))
