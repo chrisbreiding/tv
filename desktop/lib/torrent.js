e111332b93efba78692fa65251c117f0e7203a82
Chris Breiding <chrisbreiding@gmail.com>
2017-02-20 21:41:28 -0500

    try searching for specific episode
    and fall back to searching for show if no results

--- desktop/lib/torrent.js
+++ desktop/lib/torrent.js
@@ -25,51 +25,55 @@ const selectTorrent = (episode, torrents) => {
   .catch(util.notCancelationError, util.wrapHandlingError('Error selecting torrent'))
 }
 
+const search = (episode, query) => {
+  return Promise.resolve(TF.search(query, {
+    category: '205', // TV Shows
+    orderBy: 'date',
+    sortBy: 'desc',
+    filter: { verified: false },
+  }))
+  .timeout(10000)
+  .then((results) => {
+    return _(results)
+      .sortBy((result) => Number(result.seeders))
+      .reverse()
+      .value()
+  })
+}
+
 const getLink = (episode) => {
   queue.update({ id: episode.id, state: queue.SEARCHING_TORRENTS })
 
   const off = () => ipc.off(`cancel:queue:item:${episode.id}`)
 
   return new Promise((resolve, reject) => {
-    const search = TF.search(episode.show.searchName, {
-      category: '205', // TV Shows
-      orderBy: 'date',
-      sortBy: 'desc',
-      filter: { verified: false },
+    const epNum = `s${util.pad(episode.season)}e${util.pad(episode.episode_number)}`
+    search(episode, `${episode.show.searchName} ${epNum}`)
+    .then((results) => {
+      return results.length ? results : search(episode, episode.show.searchName)
     })
-    Promise.resolve(search)
-    .then((results) => { off(); resolve(results) })
-    .catch((error) => { off(); reject(error) })
+    .then((results) => resolve(results))
+    .catch((error) => reject(error))
 
     ipc.once(`cancel:queue:item:${episode.id}`, () => {
       queue.update({ id: episode.id, state: queue.CANCELING })
       reject(new util.CancelationError('Canceled searching for torrents'))
     })
   })
-  .timeout(10000)
   .then((results) => {
-    let matches = _(results)
-      .filter((result) => util.matchesEpisodeName(episode, result.name))
-      .sortBy((result) => Number(result.seeders))
-      .reverse()
-      .value()
-
-    if (matches.length) {
-      if (Number(matches[0].seeders) > 100) {
-        return matches[0].magnetLink
-      }
-    } else {
-      matches = _(results)
-        .sortBy((result) => Number(result.seeders))
-        .reverse()
-        .value()
-    }
-
-    if (!matches.length) {
+    if (!results.length) {
       throw new util.HandlingError('Could not find any torrents for episode')
     }
 
-    return selectTorrent(episode, matches)
+    const firstResult = results[0]
+    if (
+      util.matchesEpisodeName(episode, firstResult.name) &&
+      (results.length === 1 || Number(firstResult.seeders) > 100)
+    ) {
+      return firstResult.magnetLink
+    } else {
+      return selectTorrent(episode, results)
+    }
   })
   .catch(Promise.TimeoutError, util.wrapHandlingError('Timed out searching for torrents'))
   .catch(util.notCancelationError, util.wrapHandlingError('Error searching for torrents'))
