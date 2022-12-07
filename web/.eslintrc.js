af05b3ca6f7c53d2432df351fc8e1c623864ca57
Chris Breiding <chrisbreiding@gmail.com>
2022-12-07 00:08:30 -0500

    better no-unused-vars

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -7,6 +7,12 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', {
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_',
+      'caughtErrorsIgnorePattern': '^_',
+    }],
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
